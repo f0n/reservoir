843b0769e73208e436fb5589ef80598831674a46
f0n <fonseca414@gmail.com>
2016-05-23 09:25:08 -0500

    first commit

---
+++ reservoir.py
@@ -0,0 +1,12 @@
+import numpy as np
+import matplotlib.pyplot as plt
+#%matplotlib inline
+plt.clf()
+
+duration = 10.
+dt = 0.1
+N = int(duration/dt)
+
+#Process noise
+noise1 = 9797185771745235
+noise2 = 2485205224588954
